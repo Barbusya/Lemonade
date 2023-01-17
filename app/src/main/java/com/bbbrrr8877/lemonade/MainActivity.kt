8c65730dbfa37afd14b1ec6a997c3336debff61d
bulat <bbbrrr8877@gmail.com>
2023-01-17 23:47:14 +0300

    click

--- app/src/main/java/com/bbbrrr8877/lemonade/MainActivity.kt
+++ app/src/main/java/com/bbbrrr8877/lemonade/MainActivity.kt
@@ -3,13 +3,23 @@ package com.bbbrrr8877.lemonade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.bbbrrr8877.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,26 +27,59 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LemonadeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting()
-                }
+                LemonTree()
             }
         }
     }
 }
 
 @Composable
-fun Greeting() {
+fun LemonTree(
+    modifier: Modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+) {
+    var image by remember { mutableStateOf(1) }
+    val imageModifier = Modifier
+        .border(BorderStroke(3.dp, Color(R.color.image_border)))
+        .clickable {
+            when(image) {
+                1 -> image++
+                2 -> image += (0..1).random()
+                3 -> image++
+                else -> image = 1
+            }
+        }
+    val imageResource = 1
+     when (image) {
+        1 -> R.drawable.lemon_tree
+        2 -> R.drawable.lemon_squeeze
+        3 -> R.drawable.lemon_drink
+        else -> R.drawable.lemon_restart
+    }
+    val textResource = when (image) {
+        1 -> R.string.lemon_tree
+        2 -> R.string.lemon
+        3 -> R.string.glass_of_lemonade
+        else -> R.string.empty_glass
+    }
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(stringResource(textResource), fontSize = 20.sp)
+        Spacer(modifier = Modifier.height(16.dp))
+        Image(
+            painter = painterResource(id = imageResource),
+            contentDescription = image.toString(),
+            modifier = imageModifier
+            )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    LemonadeTheme {
-        Greeting()
-    }
+fun LemonadePreview() {
+    LemonTree()
 }
